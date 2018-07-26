c76b1dedc702500ac3fa92f02cd7de78bc49d426
Timothy Dao <timster78811@gmail.com>
2018-07-26 14:32:14 -0400

    updated index.js

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+'use strict';
+/* global $, bookmarkList, store, api */
+
+$(document).ready(function() {
+  bookmarkList.bindEventListeners();
+  bookmarkList.render();
+  api.getItems((items) => {
+    items.forEach((item) => store.addItem(item));
+    bookmarkList.render();
+  });
+  
+});
